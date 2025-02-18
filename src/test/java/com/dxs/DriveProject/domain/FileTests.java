db1edf7ee903984bb14f8e20eb0ffecc78a6ac66
Antoine Romand <antoine.romand@epitech.eu>
2025-02-18 20:40:12 +0100

    test file domain class

---
+++ src/test/java/com/dxs/DriveProject/domain/FileTests.java
@@ -0,0 +1,46 @@
+package com.dxs.DriveProject.domain;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class FileTests {
+
+    @Test
+    void shouldRenameFile() {
+        File file = new File("1", UUID.randomUUID(), "123", "document.txt", "/files/document.txt", false, 500L,
+                "text/plain", false, new Date());
+
+        file.renameFile("new-name.txt");
+
+        assertEquals("new-name.txt", file.getFilename());
+    }
+
+    @Test
+    void shouldMarkFileAsDeleted() {
+        File file = new File("1", UUID.randomUUID(), "123", "document.txt", "/files/document.txt", false, 500L,
+                "text/plain", false, new Date());
+
+        file.markAsDeleted();
+
+        assertTrue(file.isSoftDeleted());
+    }
+
+    @Test
+    void shouldToggleBookmark() {
+        File file = new File("1", UUID.randomUUID(), "123", "document.txt", "/files/document.txt", false, 500L,
+                "text/plain", false, new Date());
+
+        file.toggleBookmark();
+
+        assertTrue(file.isBookmarked());
+
+        file.toggleBookmark();
+
+        assertFalse(file.isBookmarked());
+    }
+}
