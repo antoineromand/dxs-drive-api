a91c61a303124a38dcb8d3109ba10b83d4d95d3d
Antoine Romand <antoine.romand@epitech.eu>
2025-03-01 15:57:54 +0100

    Remove exceptions and return stacks errors. Can return partial content body (if 1 of list files cannot be uploaded).

--- src/main/java/com/dxs/DriveProject/web/controllers/FilesController.java
+++ src/main/java/com/dxs/DriveProject/web/controllers/FilesController.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.dxs.DriveProject.application.usecases.dto.UploadResponse;
+import com.dxs.DriveProject.web.controllers.dto.ApiSuccessResponse;
+import com.dxs.DriveProject.web.controllers.dto.FileDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +29,19 @@ public class FilesController {
     }
 
     @PostMapping(consumes = { "multipart/form-data" }, value = "/upload")
-    public ArrayList<File> uploadFiles(@RequestParam("userId") String userId, @RequestParam("files") List<MultipartFile> files,
-            @RequestParam(value = "folderId", required = false) String folderId) throws IOException {
-        return uploadFileUseCase.execute(files, userId, folderId);
+    public ResponseEntity<?> uploadFiles(@RequestParam(value = "files", required = false) List<MultipartFile> files, @RequestParam(value= "userId", required = false) String userId,
+                                         @RequestParam(value = "folderId", required = false) String folderId) throws IOException {
+        UploadResponse<List<FileDTO>> result = this.uploadFileUseCase.execute(files, userId, folderId);
+
+        if (result.getData().isEmpty() && !result.getErrors().isEmpty()) {
+            return ResponseEntity.badRequest().body(result.getErrors());
+        }
+
+        if (!result.getErrors().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).body(result);
+        }
+
+        return ResponseEntity.ok(new ApiSuccessResponse<>(result.getData(), "Files uploaded successfully."));
     }
 
 }
